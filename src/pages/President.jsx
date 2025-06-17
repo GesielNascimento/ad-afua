1e5ce6fd14a3dd1d64afab9388542433ac6cb1d9
GesielNascimento <143515042+GesielNascimento@users.noreply.github.com>
2025-06-17 09:58:27 -0300

    Ajuste de destaque no nome do Pastor Isaías Bentes

--- src/pages/President.jsx
+++ src/pages/President.jsx
@@ -4,7 +4,7 @@ export default function President() {
       <div className="flex flex-wrap gap-6">
         {/* Imagem à esquerda */}
         <img
-          src="/img/silas.jpg"
+          src="/img/ISAIAS BENTES.png"
           alt="Foto do Pr. João da Silva"
           className="w-48 h-48 object-cover rounded-full shadow flex-shrink-0"
         />
@@ -13,9 +13,9 @@ export default function President() {
         <div className="flex-1 min-w-[250px]">
           <h1 className="text-3xl font-bold text-green-800 mb-4">Pr. João da Silva</h1>
           <p className="text-gray-700 leading-relaxed text-justify">
-            O Pastor João da Silva é o presidente da Igreja do Avivamento desde 2005. Com mais de 30 anos de ministério,
-            tem liderado com dedicação, amor à Palavra e zelo pelas famílias. Graduado em Teologia e com pós-graduação
-            em Liderança Cristã, é conhecido por suas mensagens inspiradoras e por seu coração pastoral.
+            O <strong>Pastor Isaías Bentes</strong> é o <strong>presidente da Igreja Evangélica Assembleia de Deus em Afuá</strong> desde 2020. Com mais 
+            de 40 anos de ministério, tem liderado com dedicação, amor à Palavra e zelo pelas famílias. Graduado em Teologia 
+            e com pós-graduação em Liderança Cristã, é conhecido por suas mensagens inspiradoras e por seu coração pastoral.
             <br /><br />
             Ao longo dos anos, tornou-se referência entre os líderes da cidade, promovendo ações sociais, evangelismo
             e discipulado. Seu ministério é marcado por oração, visão clara e compromisso com a Palavra de Deus.
